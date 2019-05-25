cc4092c8a500fb778b05f7138122ccd24a3d990d
Habib ul Rehman <mhabibulrehman@gmail.com>
2019-05-25 09:56:08 +0500

    update github graph username

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,11 +60,11 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    new GitHubCalendar("#github-graph", "IonicaBizau");
+    new GitHubCalendar("#github-graph", "habib-developer");
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "mdo", selector: "#ghfeed" });
+    GitHubActivity.feed({ username: "habib-developer", selector: "#ghfeed" });
 
 
 });
